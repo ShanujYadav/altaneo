0fcb01fe80ded11f0220b432f7f63b43de92cea6
Shanuj Yadav <shanujyadav@gmail.com>
2024-12-07 15:47:36 +0530

    add dummy data

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import dotenv from 'dotenv'
 import express from 'express'
+import cors from 'cors'
 const app = express();
 
 
 dotenv.config({
     path: '../env'
 })
+
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
@@ -16,9 +19,26 @@ app.get('/api/sanu', (req, res) => {
     res.send(`${process.env.NAME}`)
 })
 
+const dummyData = [
+    {
+      id: 1,
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      phone: '123-456-7890',
+      message: 'Looking forward to working with you!',
+    },
+    {
+      id: 2,
+      name: 'Jane Smith',
+      email: 'jane.smith@example.com',
+      phone: '987-654-3210',
+      message: 'I am interested in your services.',
+    },
+  ];
+  
 
-app.get('/api/manu', (req, res) => {
-    res.send('ho gya bhai !')
+app.get('/api/data', (req, res) => {
+    res.status(200).json(dummyData);
 })
 
 
