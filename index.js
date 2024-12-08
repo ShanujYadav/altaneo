a9078694cd8450921fcdd5aa75467d2c4f095dd7
Shanuj Yadav <shanujyadav@gmail.com>
2024-12-08 09:51:03 +0530

    connnect db

--- index.js
+++ index.js
@@ -2,46 +2,23 @@ import dotenv from 'dotenv'
 import { createHash, createHmac } from "crypto-browserify"
 import express from 'express'
 import cors from 'cors'
+import connectDB from './src/db/connectDB.js';
 const app = express();
 dotenv.config()
-
 
 const hmacKey=process.env.HMACKEY
 const pid=process.env.PID
 
-
 app.use(cors())
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
 
 
 app.get('/api/sanu', (req, res) => {
-  res.send(`${process.env.NAME}`)
+  res.send(`${process.env.PID}`)
 })
 
 
-const dummyData = [
-  {
-    id: 1,
-    name: 'John Doe',
-    email: 'john.doe@example.com',
-    phone: '123-456-7890',
-    message: 'Looking forward to working with you!',
-  },
-  {
-    id: 2,
-    name: 'Jane Smith',
-    email: 'jane.smith@example.com',
-    phone: '987-654-3210',
-    message: 'I am interested in your services.',
-  },
-];
-
 
 let ymd = '20241207'
-
 const getHmac = (pid, ymd) => {
   console.log('pid---', pid);
   console.log('ymd---', ymd);
@@ -49,16 +26,19 @@ const getHmac = (pid, ymd) => {
   var hmac = createHmac("sha256", hmacKey).update(value).digest("hex")
   return hmac.toString("base64")
 }
-
 console.log('hmac---', getHmac(pid,ymd))
 
 
-app.get('/api/data', (req, res) => {
-  res.status(200).json(dummyData);
-})
 
 
 
-app.listen(process.env.PORT || 8000, () => {
-  console.log(`Server is running at ${process.env.PORT}`);
-})
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Database connection failed:', err.message);
+    process.exit(1); 
+  });
